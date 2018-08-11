e83ea19e649deef624e2aeb05ef68212e4ea10d3
Simon Taddiken <simon.taddiken@gmail.com>
2018-08-11 09:57:39 +0200

    Simplify: remove ConsistencyChecher

--- src/main/java/de/skuzzle/enforcer/restrictimports/analyze/BannedImportGroup.java
+++ src/main/java/de/skuzzle/enforcer/restrictimports/analyze/BannedImportGroup.java
@@ -147,8 +147,48 @@ public final class BannedImportGroup {
             final BannedImportGroup group = new BannedImportGroup(basePackages,
                     bannedImports, allowedImports,
                     excludedClasses, reason);
-            GroupConsistencyChecker.getInstance().checkGroupConsistency(group);
+            checkGroupConsistency(group);
             return group;
+        }
+
+        private void checkGroupConsistency(BannedImportGroup group)
+                throws EnforcerRuleException {
+            checkBannedImportsPresent(group);
+            allowedImportMustMatchBannedPattern(group);
+            exclusionsMustMatchBasePattern(group);
+        }
+
+        private void checkBannedImportsPresent(BannedImportGroup group)
+                throws EnforcerRuleException {
+            if (group.getBannedImports().isEmpty()) {
+                throw new EnforcerRuleException("There are no banned imports specified");
+            }
+        }
+
+        private void allowedImportMustMatchBannedPattern(BannedImportGroup group)
+                throws EnforcerRuleException {
+            for (final PackagePattern allowedImport : group.getAllowedImports()) {
+                final boolean matches = group.getBannedImports().stream()
+                        .anyMatch(bannedPackage -> bannedPackage.matches(allowedImport));
+                if (!matches) {
+                    throw new EnforcerRuleException(String.format(
+                            "The allowed import pattern '%s' does not match any banned package.",
+                            allowedImport));
+                }
+            }
+        }
+
+        private void exclusionsMustMatchBasePattern(BannedImportGroup group)
+                throws EnforcerRuleException {
+            for (final PackagePattern excludedClass : group.getExcludedClasses()) {
+                final boolean matches = group.getBasePackages().stream()
+                        .anyMatch(basePackage -> basePackage.matches(excludedClass));
+                if (!matches) {
+                    throw new EnforcerRuleException(String.format(
+                            "The exclusion pattern '%s' does not match any base package.",
+                            excludedClass));
+                }
+            }
         }
     }
 }
--- src/test/java/de/skuzzle/enforcer/restrictimports/analyze/BannedImportGroupTest.java
+++ src/test/java/de/skuzzle/enforcer/restrictimports/analyze/BannedImportGroupTest.java
@@ -1,5 +1,8 @@
 package de.skuzzle.enforcer.restrictimports.analyze;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
 import org.junit.jupiter.api.Test;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
@@ -10,4 +13,42 @@ public class BannedImportGroupTest {
     public void testEquals() throws Exception {
         EqualsVerifier.forClass(BannedImportGroup.class).verify();
     }
+
+    @Test
+    void testNoBannedImports() throws Exception {
+        assertThatExceptionOfType(EnforcerRuleException.class)
+                .isThrownBy(() -> BannedImportGroup.builder().build());
+    }
+
+    @Test
+    void testInconsistentAllowedImports() throws Exception {
+        assertThatExceptionOfType(EnforcerRuleException.class)
+                .isThrownBy(() -> BannedImportGroup.builder()
+                        .withBannedImports("dont.care")
+                        .withBasePackages("com.foo.*")
+                        .withAllowedImports("foo.**")
+                        .build());
+
+    }
+
+    @Test
+    void testInconsistentExclusions() throws Exception {
+        assertThatExceptionOfType(EnforcerRuleException.class)
+                .isThrownBy(() -> BannedImportGroup.builder()
+                        .withBannedImports("dont.care")
+                        .withBasePackages("base.package.**")
+                        .withAllowedImports("foo.bar.**")
+                        .build());
+    }
+
+    @Test
+    void testExclusionsMustMatchBasePattern() throws Exception {
+        assertThatExceptionOfType(EnforcerRuleException.class)
+                .isThrownBy(() -> BannedImportGroup.builder()
+                        .withBasePackages("de.skuzzle.**")
+                        .withBannedImports("foo.bar")
+                        .withExcludedClasses("de.not.skuzzle.**")
+                        .build());
+    }
+
 }

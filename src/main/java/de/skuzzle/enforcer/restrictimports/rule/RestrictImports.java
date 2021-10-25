f50a8bca37dae6654bae8a0533803d88352f104e
Simon Taddiken <simon.taddiken@gmail.com>
2021-10-25 08:49:18 +0200

    Repeat the deprecation warning after the build

--- src/main/java/de/skuzzle/enforcer/restrictimports/rule/RestrictImports.java
+++ src/main/java/de/skuzzle/enforcer/restrictimports/rule/RestrictImports.java
@@ -11,17 +11,22 @@ import org.slf4j.LoggerFactory;
 @Deprecated
 public class RestrictImports extends org.apache.maven.plugins.enforcer.RestrictImports {
 
+    private static final String DEPRECATION_WARNING = "You are using a deprecated declaration of the "
+            + "RestrictImports enforcer rule (Deprecated since 1.4.0). Instead of "
+            + "<restrictImports implementation=\"de.skuzzle.enforcer.restrictimports.rule.RestrictImports\"> you can "
+            + "now simplify the decalration to just <RestrictImports>. The deprecated declaration format will be "
+            + "removed with the next major release (2.x.x)";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(RestrictImports.class);
 
     @Override
     public void execute(EnforcerRuleHelper helper) throws EnforcerRuleException {
-        LOGGER.warn(
-                "You are using a deprecated declaration of the RestrictImports enforcer rule (Deprecated since 1.4.0). "
-                        + "Instead of "
-                        + "<restrictImports implementation=\"de.skuzzle.enforcer.restrictimports.rule.RestrictImports\"> you "
-                        + "can now simplify the decalration to just <RestrictImports>. The deprecated declaration "
-                        + "format will be removed with the next major release (2.x.x)");
-        super.execute(helper);
+        LOGGER.warn(DEPRECATION_WARNING);
+        try {
+            super.execute(helper);
+        } finally {
+            LOGGER.warn(DEPRECATION_WARNING);
+        }
     }
 
 }

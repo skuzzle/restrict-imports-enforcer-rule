ab3fc639b86043bc3f550120df839a862cb0f879
Simon Taddiken <simon.taddiken@gmail.com>
2025-03-24 11:11:33 +0100

    Fail with a proper message in case 2 groups are using same basePackages

--- restrict-imports-enforcer-rule-core/src/test/java/de/skuzzle/enforcer/restrictimports/analyze/BannedImportGroupsTest.java
+++ restrict-imports-enforcer-rule-core/src/test/java/de/skuzzle/enforcer/restrictimports/analyze/BannedImportGroupsTest.java
@@ -1,5 +1,7 @@
 package de.skuzzle.enforcer.restrictimports.analyze;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 import org.junit.jupiter.api.Test;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
@@ -9,5 +11,32 @@ public class BannedImportGroupsTest {
     @Test
     void testEquals() throws Exception {
         EqualsVerifier.forClass(BannedImportGroups.class).verify();
+    }
+
+    @Test
+    void testSingleAmbiguousBasePackages() throws Exception {
+        assertThatExceptionOfType(BannedImportDefinitionException.class)
+                .isThrownBy(() -> BannedImportGroups.builder().withGroup(BannedImportGroup.builder()
+                        .withBasePackages("java.util.**")
+                        .withBannedImports("any.thing"))
+                        .withGroup(BannedImportGroup.builder()
+                                .withBasePackages("java.util.**")
+                                .withBannedImports("any.thing"))
+                        .build())
+                .withMessageContaining("There are two groups with equal base package definitions: java.util.**");
+    }
+
+    @Test
+    void testMultipleAmbiguousBasePackagesWithDifferentOrder() throws Exception {
+        assertThatExceptionOfType(BannedImportDefinitionException.class)
+                .isThrownBy(() -> BannedImportGroups.builder().withGroup(BannedImportGroup.builder()
+                        .withBasePackages("java.util.**", "com.foo.*")
+                        .withBannedImports("any.thing"))
+                        .withGroup(BannedImportGroup.builder()
+                                .withBasePackages("com.foo.*", "java.util.**")
+                                .withBannedImports("any.thing"))
+                        .build())
+                .withMessageContaining(
+                        "There are two groups with equal base package definitions: com.foo.*, java.util.**");
     }
 }
